47534a52ac69f208cef928859d2c3de052b4e6f4
abar1 <abdelatif.arafa@mondia.com>
2022-06-07 23:39:17 +0200

    test

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://deploytest-env.eba-bpwwjqnj.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://deploytest-env.eba-bpwwjqnj.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://deploytest-env.eba-bpwwjqnj.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://deploytest-env.eba-bpwwjqnj.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
